95f1c5d156aec2b952e7238f1fb5e0ba97fd6ec9
niroj925 <ngtc0925@gmail.com>
2024-12-16 13:00:12 +0545

    ch sth

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -6,13 +6,4 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
-
- async getNepseData(){
-    try {
-      const response = await axios.get('https://api.nepsetrading.com/stocks-listed');
-      return response.data; // Forward data to the frontend
-    } catch (error) {
-      throw error
-    }
-  }
 }
